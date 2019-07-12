973ea5d3b526facfb78e25d6189a7544d7020475
Daniel Moran <danmoran@broadinstitute.org>
2019-07-12 14:48:54 -0400

    Redesign GcsApi's HTTP arg to return a Resource. (#2)

--- gcs/src/main/scala/org/broadinstitute/monster/storage/gcs/GcsApi.scala
+++ gcs/src/main/scala/org/broadinstitute/monster/storage/gcs/GcsApi.scala
@@ -3,7 +3,7 @@ package org.broadinstitute.monster.storage.gcs
 import java.nio.file.Path
 
 import cats.data.NonEmptyList
-import cats.effect.{Clock, ContextShift, IO}
+import cats.effect.{Clock, ContextShift, IO, Resource}
 import fs2.Stream
 import org.http4s._
 import org.http4s.client.Client
@@ -12,15 +12,10 @@ import org.http4s.headers.{`Accept-Encoding`, `Content-Encoding`}
 /**
   * Client which can perform I/O operations against Google Cloud Storage.
   *
-  * @param authProvider utility which can add GCS-specific authorization headers
-  *                     to outgoing HTTP requests
   * @param runHttp function which can transform HTTP requests into HTTP responses
   *                (bracketed by connection-management code)
   */
-class GcsApi private[gcs] (
-  authProvider: GcsAuthProvider,
-  runHttp: Request[IO] => Stream[IO, Response[IO]]
-) {
+class GcsApi private[gcs] (runHttp: Request[IO] => Resource[IO, Response[IO]]) {
   import GcsApi._
 
   /**
@@ -69,49 +64,67 @@ class GcsApi private[gcs] (
       headers = Headers.of(range, accept)
     )
 
-    Stream
-      .eval(authProvider.addAuth(request))
-      .flatMap(runHttp)
-      .flatMap { response =>
-        if (response.status.isSuccess && gunzipIfNeeded) {
-          response.headers.get(`Content-Encoding`) match {
-            case Some(header)
-                if header.contentCoding == ContentCoding.gzip || header.contentCoding == ContentCoding.`x-gzip` =>
-              response.body.through(fs2.compress.gunzip(GunzipBufferSize))
-            case _ => response.body
-          }
-        } else if (response.status.isSuccess) {
-          response.body
-        } else {
-          val fullBody =
-            response.body.compile.toChunk.map(chunk => new String(chunk.toArray[Byte]))
-          Stream.eval(fullBody).flatMap { payload =>
-            val err =
-              s"""Attempt to get object bytes returned ${response.status}:
+    Stream.resource(runHttp(request)).flatMap { response =>
+      if (response.status.isSuccess && gunzipIfNeeded) {
+        response.headers.get(`Content-Encoding`) match {
+          case Some(header)
+              if header.contentCoding == ContentCoding.gzip || header.contentCoding == ContentCoding.`x-gzip` =>
+            response.body.through(fs2.compress.gunzip(GunzipBufferSize))
+          case _ => response.body
+        }
+      } else if (response.status.isSuccess) {
+        response.body
+      } else {
+        val fullBody =
+          response.body.compile.toChunk.map(chunk => new String(chunk.toArray[Byte]))
+        Stream.eval(fullBody).flatMap { payload =>
+          val err =
+            s"""Attempt to get object bytes returned ${response.status}:
                |$payload""".stripMargin
-            Stream.raiseError[IO](new Exception(err))
-          }
+          Stream.raiseError[IO](new Exception(err))
         }
       }
+    }
   }
 }
 
 object GcsApi {
 
   /**
-    * Construct a GCS client which will delegate to a given HTTP client when sending requests,
-    * optionally using service account credentials for authorization instead of the environment's
-    * application default credentials.
+    * Construct a client which will send authorized requests to GCS over HTTP.
     *
-    * @param httpClient client which will actually send / receive HTTP requests to / from GCS
-    * @param serviceAccountJson optional path to service account credentials on disk, to use
-    *                           in place of application default credentials
+    * All connection management / internal HTTP logic is handled by the client
+    * passed as input to the builder, allowing users to inject arbitrary implementations
+    * or middleware as needed.
+    *
+    * If a path to service account JSON is given, that account will be used to
+    * build authorization headers for outgoing requests. Otherwise the environment's
+    * application-default credentials will be used.
+    *
+    * @param httpClient client which will actually send HTTP requests / parse responses
+    * @param serviceAccountJson path to service account credentials to use in place
+    *                           of application default credentials
     */
   def build(
     httpClient: Client[IO],
     serviceAccountJson: Option[Path]
   )(implicit cs: ContextShift[IO], clk: Clock[IO]): IO[GcsApi] =
-    GcsAuthProvider.build(serviceAccountJson).map(new GcsApi(_, httpClient.stream))
+    GcsAuthProvider.build(serviceAccountJson).map { auth =>
+      /*
+       * NOTE: Injecting the call to `.addAuth` here instead of in the class body
+       * has the following trade-off:
+       *
+       *   1. It prevents the auth-adding logic from being covered by unit tests, BUT
+       *   2. It makes it impossible for the business logic in the class body to
+       *      "forget" to add auth, so as long as we get the logic correct here we
+       *      can be confident that HTTP calls in the client won't fail from that
+       *      particular bug
+       *
+       * 2. is a huge win for code cleanliness and long-term maintenance. Our integration
+       * tests will thoroughly cover this builder method, so 1. isn't that big of a problem.
+       */
+      new GcsApi(req => Resource.liftF(auth.addAuth(req)).flatMap(httpClient.run))
+    }
 
   /** Build the JSON API endpoint for an existing bucket/path in GCS. */
   def baseGcsUri(bucket: String, path: String): Uri =
