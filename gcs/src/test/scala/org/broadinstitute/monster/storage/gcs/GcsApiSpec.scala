dcf33b48ec87e71d3065f8c4a11668140c64ec72
Dan Moran <danmoran@broadinstitute.org>
2019-07-11 20:53:54 -0400

    Redesign GcsApi's HTTP arg to return a Resource.

--- gcs/src/test/scala/org/broadinstitute/monster/storage/gcs/GcsApiSpec.scala
+++ gcs/src/test/scala/org/broadinstitute/monster/storage/gcs/GcsApiSpec.scala
@@ -1,9 +1,8 @@
 package org.broadinstitute.monster.storage.gcs
 
-import cats.effect.IO
+import cats.effect.{IO, Resource}
 import fs2.Stream
 import org.http4s._
-import org.http4s.headers.Authorization
 import org.scalatest.{FlatSpec, Matchers}
 
 class GcsApiSpec extends FlatSpec with Matchers {
@@ -15,23 +14,17 @@ class GcsApiSpec extends FlatSpec with Matchers {
   private val bodyText = "Some example body text"
   private val bodyStream = Stream.emits(bodyText.getBytes.toSeq).covary[IO]
 
-  private val fakeAuth = Authorization(BasicCredentials("foo", "bar"))
-
-  private val auth: GcsAuthProvider =
-    (req: Request[IO]) => IO.pure(req.transformHeaders(_.put(fakeAuth)))
-
   behavior of "GcsApi"
 
   it should "read entire objects as a stream" in {
-    val api = new GcsApi(auth, req => {
+    val api = new GcsApi(req => {
       req.method shouldBe Method.GET
       req.uri shouldBe getUri
       req.headers.toList should contain theSameElementsAs List(
-        fakeAuth,
         Header("Accept-Encoding", "identity, gzip"),
         Header("Range", "bytes=0-")
       )
-      Stream.emit(Response[IO](body = bodyStream))
+      Resource.pure(Response[IO](body = bodyStream))
     })
 
     api
@@ -44,15 +37,14 @@ class GcsApiSpec extends FlatSpec with Matchers {
   }
 
   it should "gunzip compressed data if told to" in {
-    val api = new GcsApi(auth, req => {
+    val api = new GcsApi(req => {
       req.method shouldBe Method.GET
       req.uri shouldBe getUri
       req.headers.toList should contain theSameElementsAs List(
-        fakeAuth,
         Header("Accept-Encoding", "identity, gzip"),
         Header("Range", "bytes=0-")
       )
-      Stream.emit {
+      Resource.pure {
         Response[IO](
           body = bodyStream.through(fs2.compress.gzip(GcsApi.GunzipBufferSize)),
           headers = Headers.of(Header("Content-Encoding", "gzip"))
@@ -70,15 +62,14 @@ class GcsApiSpec extends FlatSpec with Matchers {
   }
 
   it should "not gunzip uncompressed data" in {
-    val api = new GcsApi(auth, req => {
+    val api = new GcsApi(req => {
       req.method shouldBe Method.GET
       req.uri shouldBe getUri
       req.headers.toList should contain theSameElementsAs List(
-        fakeAuth,
         Header("Accept-Encoding", "identity, gzip"),
         Header("Range", "bytes=0-")
       )
-      Stream.emit(Response[IO](body = bodyStream))
+      Resource.pure(Response[IO](body = bodyStream))
     })
 
     api
@@ -92,15 +83,14 @@ class GcsApiSpec extends FlatSpec with Matchers {
 
   it should "read objects starting at an offset" in {
     val start = 100L
-    val api = new GcsApi(auth, req => {
+    val api = new GcsApi(req => {
       req.method shouldBe Method.GET
       req.uri shouldBe getUri
       req.headers.toList should contain theSameElementsAs List(
-        fakeAuth,
         Header("Accept-Encoding", "identity, gzip"),
         Header("Range", s"bytes=$start-")
       )
-      Stream.emit(Response[IO](body = bodyStream))
+      Resource.pure(Response[IO](body = bodyStream))
     })
 
     api
@@ -114,15 +104,14 @@ class GcsApiSpec extends FlatSpec with Matchers {
 
   it should "read objects ending before the final byte" in {
     val end = 1000L
-    val api = new GcsApi(auth, req => {
+    val api = new GcsApi(req => {
       req.method shouldBe Method.GET
       req.uri shouldBe getUri
       req.headers.toList should contain theSameElementsAs List(
-        fakeAuth,
         Header("Accept-Encoding", "identity, gzip"),
         Header("Range", s"bytes=0-${end - 1}")
       )
-      Stream.emit(Response[IO](body = bodyStream))
+      Resource.pure(Response[IO](body = bodyStream))
     })
 
     api
@@ -137,15 +126,14 @@ class GcsApiSpec extends FlatSpec with Matchers {
   it should "read slices in the middle of an object" in {
     val start = 100L
     val end = 1000L
-    val api = new GcsApi(auth, req => {
+    val api = new GcsApi(req => {
       req.method shouldBe Method.GET
       req.uri shouldBe getUri
       req.headers.toList should contain theSameElementsAs List(
-        fakeAuth,
         Header("Accept-Encoding", "identity, gzip"),
         Header("Range", s"bytes=$start-${end - 1}")
       )
-      Stream.emit(Response[IO](body = bodyStream))
+      Resource.pure(Response[IO](body = bodyStream))
     })
 
     api
@@ -163,16 +151,14 @@ class GcsApiSpec extends FlatSpec with Matchers {
          |
          |I broke.""".stripMargin
 
-    val api = new GcsApi(
-      auth,
-      _ =>
-        Stream.emit(
-          Response[IO](
-            status = Status.BadRequest,
-            body = Stream.emit(err).through(fs2.text.utf8Encode)
-          )
+    val api = new GcsApi({ _ =>
+      Resource.pure {
+        Response[IO](
+          status = Status.BadRequest,
+          body = Stream.emit(err).through(fs2.text.utf8Encode)
         )
-    )
+      }
+    })
 
     api
       .readObject(bucket, path)
@@ -192,16 +178,14 @@ class GcsApiSpec extends FlatSpec with Matchers {
          |
          |I broke.""".stripMargin
 
-    val api = new GcsApi(
-      auth,
-      _ =>
-        Stream.emit(
-          Response[IO](
-            status = Status.BadGateway,
-            body = Stream.emit(err).through(fs2.text.utf8Encode)
-          )
+    val api = new GcsApi({ _ =>
+      Resource.pure {
+        Response[IO](
+          status = Status.BadGateway,
+          body = Stream.emit(err).through(fs2.text.utf8Encode)
         )
-    )
+      }
+    })
 
     api
       .readObject(bucket, path)
