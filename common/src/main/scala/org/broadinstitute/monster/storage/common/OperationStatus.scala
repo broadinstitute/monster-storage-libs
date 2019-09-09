ed4d3fae0be4e916b63e2e1b1800629dab4677dc
Dan Moran <danmoran@broadinstitute.org>
2019-09-09 12:54:34 -0400

    Add scaladoc.

--- common/src/main/scala/org/broadinstitute/monster/storage/common/OperationStatus.scala
+++ common/src/main/scala/org/broadinstitute/monster/storage/common/OperationStatus.scala
@@ -4,6 +4,12 @@ import enumeratum.{Enum, EnumEntry}
 
 import scala.collection.immutable.IndexedSeq
 
+/**
+  * Convenience data type used to model the stages of performing an incremental
+  * operation against some external storage API (i.e. pulling pages of results).
+  *
+  * Only intended for use within storage-libs, should not be exposed in public APIs.
+  */
 private[storage] sealed trait OperationStatus extends EnumEntry
 
 private[storage] object OperationStatus extends Enum[OperationStatus] {
